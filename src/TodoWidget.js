fd0aad247c14cc677b719ad565bd7e33cbe6d529
Quentin <quentin.vuilliot@kaes.fr>
2014-04-01 10:57:44 +0200

    1000 todos: editable

--- src/TodoWidget.js
+++ src/TodoWidget.js
@@ -3,25 +3,33 @@ define([
 	'ksf/utils/destroy',
 	'ksf/dom/composite/CompositeBase',
 	'ksf-ui/layout/Flow',
-	'ksf-ui/widget/Label',
+	'ksf-ui/widget/editable/Date',
 	'ksf-ui/widget/editable/Checkbox',
+	'ksf-ui/widget/editable/Text',
 	'kss!./todo-widget.css'
 ], function(
 	compose,
 	destroy,
 	CompositeBase,
 	FlowLayout,
-	Label,
+	Date,
 	Checkbox,
+	Text,
 	todoStyle
 ){
 	return compose(CompositeBase, {
 		_rootFactory: function() { return new FlowLayout(); }
 	}, function(acc) {
-		this._label = new Label();
-		this._date = new Label();
+		this._label = new Text();
+		this._label.on('input', function(desc) {
+			this._todo.prop('desc').value(desc);
+		}.bind(this));
+		this._date = new Date();
+		this._date.on('input', function(date) {
+			this._todo.prop('date').value(date);
+		}.bind(this));
 		this._done = new Checkbox();
-		this._done.on('toggle', function(checked) {
+		this._done.on('input', function(checked) {
 			this._todo.prop('done').value(checked);
 		}.bind(this));
 
@@ -45,7 +53,7 @@ define([
 		},
 		_value: function(value) {
 			this._label.value(value.desc);
-			this._date.value(value.date.toLocaleDateString());
+			this._date.value(value.date);
 			this._done.value(value.done);
 		}
 	});
