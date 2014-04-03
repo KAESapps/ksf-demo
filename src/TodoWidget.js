2b221cd06ff448bd8ce2a79e2ec255c57958add4
Quentin <quentin.vuilliot@kaes.fr>
2014-04-03 10:08:13 +0200

    Widget with accessors + 2 lists

--- src/TodoWidget.js
+++ src/TodoWidget.js
@@ -1,60 +1,37 @@
 define([
 	'compose',
-	'ksf/utils/destroy',
 	'ksf/dom/composite/CompositeBase',
 	'ksf-ui/layout/Flow',
 	'ksf-ui/widget/editable/Date',
 	'ksf-ui/widget/editable/Checkbox',
-	'ksf-ui/widget/editable/Text',
+	'ksf-ui/widget/editable/ShortText',
+	'ksf-ui/widget/Button',
 	'kss!./todo-widget.css'
 ], function(
 	compose,
-	destroy,
 	CompositeBase,
 	FlowLayout,
 	Date,
 	Checkbox,
 	Text,
+	Button,
 	todoStyle
 ){
 	return compose(CompositeBase, {
 		_rootFactory: function() { return new FlowLayout(); }
-	}, function(acc) {
-		this._label = new Text();
-		this._label.on('input', function(desc) {
-			this._todo.prop('desc').value(desc);
-		}.bind(this));
-		this._date = new Date();
-		this._date.on('input', function(date) {
-			this._todo.prop('date').value(date);
-		}.bind(this));
-		this._done = new Checkbox();
-		this._done.on('input', function(checked) {
-			this._todo.prop('done').value(checked);
-		}.bind(this));
-
+	}, function(todo) {
+		var deleteBtn = new Button("x");
+		deleteBtn.on('pushed', function() {
+			todo.delete();
+		});
 		this._root.content([
-			this._label,
-			this._date,
-			this._done
+			new Text(todo.prop('desc')),
+			new Date(todo.prop('date')),
+			new Checkbox(todo.prop('done')),
+			deleteBtn
 		]);
 
-		acc && this.todo(acc);
 		todoStyle.apply(this.domNode);
-	}, {
-		todo: function(todo) {
-			destroy(this._todoCancelers);
-			this._todo = todo;
-
-			this._value(todo.value());
-			this._todoCancelers = [
-				todo.on('value', this._value.bind(this))
-			];
-		},
-		_value: function(value) {
-			this._label.value(value.desc);
-			this._date.value(value.date);
-			this._done.value(value.done);
-		}
+		this.domNode.id = todo._propName;
 	});
 });
--- src/boot.js
+++ src/boot.js
@@ -1,20 +1,25 @@
 define([
 	'compose',
+	'ksf/utils/destroy',
 	'ksf/observable/sync/store/DocumentStore',
 	'ksf-ui/list/List',
+	'ksf-ui/widget/Label',
 	'./TodoWidget'
 ], function(
 	compose,
+	destroy,
 	DocumentStore,
 	List,
+	Label,
 	TodoWidget
 ) {
 	var refDate = new Date();
 	var data = {},
 		done = true;
+	var labels = ["vélo", "velu", "voilà"];
 	for (var i = 0; i < 1000; i++) {
 		data[i] = {
-			desc: "faire les courses",
+			desc: labels[i % 3] + i,
 			done: done,
 			date: new Date(refDate - 24 * 3600 * 1000 * i)
 		};
@@ -30,20 +35,39 @@ define([
 	
 	var sorted = filtered.sort(function(value1, value2) {
 		return value1.date && value2.date && value1.date.valueOf() - value2.date.valueOf();
+		// return value1.desc === value2.desc ? 0 : value1.desc < value2.desc ? -1 : 1;
+		// return value1.desc.localeCompare(value2.desc);
 	});
 
-	var list = new (compose(List, {
+	var range = sorted.range(0, 20);
+	var range2 = sorted.range(20, 70);
+
+	var TestList = compose(List, {
 		_itemFactory: function(item) {
 			return new TodoWidget(item);
 		}
-	}))();
+	});
+	var list = new TestList(),
+		list2 = new TestList();
+	
+	var count = new Label();
+	var storeCount = store.count();
+	var storeCountUser = storeCount.withValue(function (value) {
+		count.value(value);
+	});
+	// destroy(storeCountUser);
+	
 	console.timeEnd('loading');
 	
 	console.time('list rendering');
-	list.content(sorted);
-	console.timeEnd('list rendering');
+	list.content(range);
+	list2.content(range2);
 
+	document.body.appendChild(count.domNode);
 	document.body.appendChild(list.domNode);
+	document.body.appendChild(list2.domNode);
+
+	console.timeEnd('list rendering');
 
 	window.addTask = function() {
 		console.time("Ajout d'une tâche");
@@ -52,6 +76,12 @@ define([
 			date: new Date("2010/1/1")
 		}, Math.random());
 		console.timeEnd("Ajout d'une tâche");
+	};
+
+	window.removeTask = function(id) {
+		console.time("Suppression d'une tâche");
+		store.remove(id || 999);
+		console.timeEnd("Suppression d'une tâche");
 	};
 
 	window.getSortedValue = function() {
